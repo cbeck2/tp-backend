6737ff22a300ed716049f13ba979620350451e34
cbeck6 <cbeck@esimed.fr>
2025-06-06 11:13:49 +0200

    final commit

--- api/interet.js
+++ api/interet.js
@@ -35,20 +35,4 @@ module.exports = (app, svc) => {
                 res.status(500).end()
             })
     })
-    /*app.put("/interet", async (req, res) => {
-        console.log(req.body)
-        const interet= req.body
-        if ((interet.id === undefined) || (interet.id == null) || (!svc.isValideinteret(interet))) {
-            return res.status(400).end()
-        }
-        if (await svc.dao.getById(interet.id) === undefined) {
-            return res.status(404).end()
-        }
-        svc.dao.updateinteret(interet)
-            .then(_ => res.status(200).end())
-            .catch(e => {
-                console.log(e)
-                res.status(500).end()
-            })
-    })*/
 }
