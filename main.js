2fa0ad92373f99168818ac1e8f63572e8d56faa2
cbeck <43646528+cbeck2@users.noreply.github.com>
2025-06-11 00:43:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const app = express()
 
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(morgan('dev'));
